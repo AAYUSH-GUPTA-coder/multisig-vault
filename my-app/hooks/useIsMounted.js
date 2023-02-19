15adeb7e45f5cb8517cdfa1efbdf60e77ad0dcb2
Aayush Gupta <guptaaayush432@gmail.com>
2023-02-19 12:22:09 +0530

    solved hydration problem

---
+++ my-app/hooks/useIsMounted.js
@@ -0,0 +1,11 @@
+import { useState, useHooks, useEffect } from "react";
+
+export function useIsMounted() {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  return mounted;
+}
--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -1,14 +1,16 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
 import { ConnectButton } from "@rainbow-me/rainbowkit";
+import { useIsMounted } from "../hooks/useIsMounted";
+import { useAccount } from "wagmi";
 
 export default function Home() {
+  const mounted = useIsMounted();
+  const { address } = useAccount();
+
   return (
     <div>
       <h1>Hello</h1>
       <ConnectButton />
+      {mounted ? address && <p>My Address is {address}</p> : null}
     </div>
   );
 }
